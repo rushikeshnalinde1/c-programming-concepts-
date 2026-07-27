a3da1b17436dd8d2b830ea81baea5f0ef0a6fec8
RushikeshNalinde <rushikeshnalinde1@gmail.com>
2026-07-27 19:32:15 +0530

    Add basic memory allocation example in C

---
+++ using malloc.c
@@ -0,0 +1,17 @@
+// 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Write C code here
+    int *ptr = (int*)malloc(sizeof(int));
+    if(ptr == NULL){
+        printf("memory allocation failed \n");
+        return 1;
+    }
+    printf("enter the element you want to save \n");
+    scanf("%d", &*ptr);
+    printf("%d", *ptr);
+
+    return 0;
+}
