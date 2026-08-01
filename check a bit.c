17ed05f387433dccc44c0ebb71d3a7b9954d10fa
RushikeshNalinde <rushikeshnalinde1@gmail.com>
2026-08-01 18:17:31 +0530

    Add C program to check if a bit is set

---
+++ check a bit.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int num = 10;
+    if(num & (1<<1)){
+        printf("bit is 1");
+    }else{
+        printf("bit is 0");
+    }
+
+    return 0;
+}
